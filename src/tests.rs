25ce92da01b544df5d973ed9dae52fc93740fc86
Electro98 <geny_turov@mail.ru>
2025-01-17 21:45:38 +0900

    Add new data for perft

--- src/tests.rs
+++ src/tests.rs
@@ -161,19 +161,68 @@ macro_rules! perf_tests {
 
 perf_tests! {
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-    perft_base_1: (400, 2)
-    perft_base_2: (8902, 3)
-    perft_base_3: (197_281, 4)
-    perft_base_4: (4_865_609, 5)
-    #[ignore="slow"] perft_base_5: (119_060_324, 6)
-    #[ignore="slow"] perft_base_6: (3_195_901_860, 7)
+    perft_base_2: (400, 2)
+    perft_base_3: (8902, 3)
+    perft_base_4: (197_281, 4)
+    perft_base_5: (4_865_609, 5)
+    #[ignore="slow"] perft_base_6: (119_060_324, 6)
+    #[ignore="slow"] perft_base_7: (3_195_901_860, 7)
 }
 
 perf_tests! {
     "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - "
-    perft_kiwipete_1: (2039, 2)
-    perft_kiwipete_2: (97_862, 3)
-    perft_kiwipete_3: (4_085_603, 4)
-    #[ignore="slow"] perft_kiwipete_4: (193_690_690, 5)
-    #[ignore="slow"] perft_kiwipete_5: (8_031_647_685, 6)
+    perft_kiwipete_2: (2039, 2)
+    perft_kiwipete_3: (97_862, 3)
+    perft_kiwipete_4: (4_085_603, 4)
+    #[ignore="slow"] perft_kiwipete_5: (193_690_690, 5)
+    #[ignore="slow"] perft_kiwipete_6: (8_031_647_685, 6)
+}
+
+perf_tests! {
+    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"
+    perft_pos3_1: (14, 1)
+    perft_pos3_2: (191, 2)
+    perft_pos3_3: (2812, 3)
+    perft_pos3_4: (43_238, 4)
+    perft_pos3_5: (674_624, 5)
+    perft_pos3_6: (11_030_083, 6)
+    #[ignore="slow"] perft_pos3_7: (178_633_661, 7)
+    #[ignore="slow"] perft_pos3_8: (3_009_794_393, 8)
+}
+
+perf_tests! {
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+    perft_pos4_2: (264, 2)
+    perft_pos4_3: (9467, 3)
+    perft_pos4_4: (422_333, 4)
+    #[ignore="slow"] perft_pos4_5: (15_833_292, 5)
+    #[ignore="slow"] perft_pos4_6: (706_045_033, 6)
+}
+
+perf_tests! {
+    "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"
+    perft_pos4_mirror_2: (264, 2)
+    perft_pos4_mirror_3: (9467, 3)
+    perft_pos4_mirror_4: (422_333, 4)
+    #[ignore="slow"] perft_pos4_mirror_5: (15_833_292, 5)
+    #[ignore="slow"] perft_pos4_mirror_6: (706_045_033, 6)
+}
+
+perf_tests! {
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
+    perft_pos5_1: (44, 1)
+    perft_pos5_2: (1486, 2)
+    perft_pos5_3: (62_379, 3)
+    perft_pos5_4: (2_103_487, 4)
+    #[ignore="slow"] perft_pos5_5: (89_941_194, 5)
+}
+
+perf_tests! {
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
+    perft_pos6_1: (46, 1)
+    perft_pos6_2: (2079, 2)
+    perft_pos6_3: (89_890, 3)
+    perft_pos6_4: (3_894_594, 4)
+    #[ignore="slow"] perft_pos6_5: (164_075_551, 5)
+    #[ignore="slow"] perft_pos6_6: (6_923_051_137, 6)
 }
