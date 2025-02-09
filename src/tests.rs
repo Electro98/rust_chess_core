8f706b616236f5634cf6716a79ce5cdce6b0416f
Electro98 <geny_turov@mail.ru>
2025-02-09 19:12:41 +0900

    Add new tests from 'rustic' material

--- src/tests.rs
+++ src/tests.rs
@@ -158,6 +158,19 @@ macro_rules! perf_tests {
     }
 }
 
+macro_rules! perft_suit {
+    ($($(#[$attr:meta])* $name:ident: $value:expr)*) => {
+    $(
+        #[test]
+        $(#[$attr])*
+        fn $name() {
+            let (fen_string, depth, expected) = $value;
+            assert!(perf_test(fen_string, depth, expected, true, true), "Results don't match up");
+        }
+    )*
+    };
+}
+
 perf_tests! {
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     perft_base_2: (400, 2)
@@ -224,4 +237,109 @@ perf_tests! {
     perft_pos6_4: (3_894_594, 4)
     #[ignore="slow"] perft_pos6_5: (164_075_551, 5)
     #[ignore="slow"] perft_pos6_6: (6_923_051_137, 6)
+}
+
+// Specials
+perft_suit! {
+    special_1: ("3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 6, 1134888)
+    special_2: ("r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1", 4, 1274206)
+    special_3: ("8/8/8/8/k1p4R/8/3P4/3K4 w - - 0 1", 6, 1134888)
+    special_4: ("8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", 6, 1440467)
+    special_5: ("8/5k2/8/2Pp4/2B5/1K6/8/8 w - d6 0 1", 6, 1440467)
+    special_6: ("8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", 6, 1015133)
+    special_7: ("8/b2p2k1/8/2P5/8/4K3/8/8 b - - 0 1", 6, 1015133)
+    special_8: ("5k2/8/8/8/8/8/8/4K2R w K - 0 1", 6, 661072)
+    special_9: ("4k2r/8/8/8/8/8/8/5K2 b k - 0 1", 6, 661072)
+    special_10: ("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 803711)
+    special_11: ("r3k3/8/8/8/8/8/8/3K4 b q - 0 1", 6, 803711)
+}
+
+// en passant capture checks opponent
+perft_suit! {
+    enpassant_check_1: ("8/8/1k6/2b5/2pP4/8/5K2/8 b - d3 0 1", 6, 1440467)
+    enpassant_check_2: ("8/5k2/8/2Pp4/2B5/1K6/8/8 w - d6 0 1", 6, 1440467)
+}
+
+// avoid illegal ep(thanks to Steve Maughan)
+perft_suit! {
+    illegal_ep_1: ("3k4/3p4/8/K1P4r/8/8/8/8 b - - 0 1", 6, 1134888)
+    illegal_ep_2: ("8/8/8/8/k1p4R/8/3P4/3K4 w - - 0 1", 6, 1134888)
+    illegal_ep_3: ("8/8/4k3/8/2p5/8/B2P2K1/8 w - - 0 1", 6, 1015133)
+    illegal_ep_4: ("8/b2p2k1/8/2P5/8/4K3/8/8 b - - 0 1", 6, 1015133)
+}
+
+// short castling gives check
+perft_suit! {
+    short_castling_check_1: ("5k2/8/8/8/8/8/8/4K2R w K - 0 1", 6, 661072)
+    short_castling_check_2: ("4k2r/8/8/8/8/8/8/5K2 b k - 0 1", 6, 661072)
+}
+
+// long castling gives check
+perft_suit! {
+    long_castling_check_1: ("3k4/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 803711)
+    long_castling_check_2: ("r3k3/8/8/8/8/8/8/3K4 b q - 0 1", 6, 803711)
+}
+
+// castling(including losing cr due to rook capture)
+perft_suit! {
+    castling_1: ("r3k2r/1b4bq/8/8/8/8/7B/R3K2R w KQkq - 0 1", 4, 1274206)
+    castling_2: ("r3k2r/7b/8/8/8/8/1B4BQ/R3K2R b KQkq - 0 1", 4, 1274206)
+}
+
+// castling prevented
+perft_suit! {
+    castling_prevented_1: ("r3k2r/8/3Q4/8/8/5q2/8/R3K2R b KQkq - 0 1", 4, 1720476)
+    castling_prevented_2: ("r3k2r/8/5Q2/8/8/3q4/8/R3K2R w KQkq - 0 1", 4, 1720476)
+}
+//  promote out of check
+perft_suit! {
+    promote_out_of_check_1: ("2K2r2/4P3/8/8/8/8/8/3k4 w - - 0 1", 6, 3821001)
+    promote_out_of_check_2: ("3K4/8/8/8/8/8/4p3/2k2R2 b - - 0 1", 6, 3821001)
+}
+
+// "# discovered check
+perft_suit! {
+    discovered_check_1: ("8/8/1P2K3/8/2n5/1q6/8/5k2 b - - 0 1", 5, 1004658)
+    discovered_check_2: ("5K2/8/1Q6/2N5/8/1p2k3/8/8 w - - 0 1", 5, 1004658)
+}
+
+// "# promote to give check
+perft_suit! {
+    promote_check_1: ("4k3/1P6/8/8/8/8/K7/8 w - - 0 1", 6, 217342)
+    promote_check_2: ("8/k7/8/8/8/8/1p6/4K3 b - - 0 1", 6, 217342)
+}
+
+// "# underpromote to check
+perft_suit! {
+    underpromote_check_1: ("8/P1k5/K7/8/8/8/8/8 w - - 0 1", 6, 92683)
+    underpromote_check_2: ("8/8/8/8/8/k7/p1K5/8 b - - 0 1", 6, 92683)
+}
+
+// "# self stalemate
+perft_suit! {
+    self_stalemate_1: ("K1k5/8/P7/8/8/8/8/8 w - - 0 1", 6, 2217)
+    self_stalemate_2: ("8/8/8/8/8/p7/8/k1K5 b - - 0 1", 6, 2217)
+}
+
+// stalemate/checkmate:
+perft_suit! {
+    stalemate_checkmate_1: ("8/k1P5/8/1K6/8/8/8/8 w - - 0 1", 7, 567584)
+    stalemate_checkmate_2: ("8/8/8/8/1k6/8/K1p5/8 b - - 0 1", 7, 567584)
+}
+
+// double check
+perft_suit! {
+    double_check_1: ("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", 4, 23527)
+}
+
+// short castling impossible although the rook never moved away from its corner
+perft_suit! {
+    impossible_short_castling_1: ("1k6/1b6/8/8/7R/8/8/4K2R b K - 0 1", 5, 1063513)
+    impossible_short_castling_2: ("4k2r/8/8/7r/8/8/1B6/1K6 w k - 0 1", 5, 1063513)
+}
+
+// long castling impossible although the rook never moved away from its corner
+perft_suit! {
+    impossible_long_castling_1: ("1k6/8/8/8/R7/1n6/8/R3K3 b Q - 0 1", 5, 346695)
+    impossible_long_castling_2: ("r3k3/8/1N6/r7/8/8/8/1K6 w q - 0 1", 5, 346695)
 }
